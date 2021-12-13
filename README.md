970b7fa516e59af7edef61b922a4506d168ce20d
muhammad <fadi.anony@protonmail.ch>
2021-12-13 13:29:19 +0200

    pushing the files

---
+++ README.md
@@ -0,0 +1,41 @@
+# Community
+> منصه للتوصيل التشاركي  
+
+
+## متطلبات عمل التطبيق
+
+1. **PostgreSQL and PostGIS**
+    - [Download](https://www.postgresql.org/download/) .
+
+    - [Install](https://postgis.net/install/).
+
+2. **`python3.7` and `pip3`**
+
+3. **[GDAL](https://gdal.org/)**
+
+    - `sudo apt-get install libpq-dev python-dev`
+    - `sudo apt-get install binutils libproj-dev gdal-bin`
+
+4. **Django and other dependencies**
+    `pip install -r requirements.text`
+
+## كيفية البدء
+
+1. **انشاء قاعدة بيانات**
+    
+
+2. **create api in mapbox and add it maps.js**
+    
+
+3. **واخيرا شغل الاوامر التاليه**
+    `python manage.py makemigrations stores`
+    `python manage.py runserver`
+
+
+
+
+# credits to:
+    (https://egen.solutions/articles/thinking-of-building-a-contact-tracing-application-what-to-do-instead/).
+
+
+# ساهم في المشروع?
---
+++ home/static/home/modules/map.js
@@ -0,0 +1,130 @@
+import {
+    updateSelectedStore,
+} from './stores.js';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibXVoYW1tYWRlbGFrZGVyIiwiYSI6ImNreDM4aGY0NTA2bXYybnAwcnZ5eTc2Y3IifQ.iS_F4DUqwDNGYFOuin46Xg';
+
+/**
+ * @typedef {import('./api').Store} Store
+ */
+
+/**
+ * Stores GeoJSON Feature object
+ * @typedef {Object} StoreFeatureObject
+ * @property {'Feature'} type
+ * @property {{type: 'Point', coordinates: [number, number] }} geometry
+ * @property {Store} properties
+ */
+
+/**
+ * Stores GeoJSON FeatureCollection
+ * @typedef {Object} StoresGeoJSON
+ * @property {'FeatureCollection'} type
+ * @property {StoreFeatureObject[]} features
+ */
+
+/**
+ * Create a new mapbox map instance
+ * @return {Object} Map
+ */
+export function addMap() {
+    const map = new mapboxgl.Map({  
+        container: 'map',  
+        style: 'mapbox://styles/mapbox/light-v10',  
+        center: [77.645296, 12.978624],  
+        zoom: 2  
+    });
+    
+    map.addControl(new mapboxgl.NavigationControl());  
+    
+    return map;
+}
+
+/**
+ * Add a geoCoder control to a mapbox map
+ * @param {Object} map
+ * @param {function} geocoderCallback - The callback that handles the response.
+ */
+export function addGeocoder(map, geocoderCallback) {
+    const geocoder = new MapboxGeocoder({ accessToken: mapboxgl.accessToken, mapboxgl: mapboxgl });map.addControl(geocoder);geocoder.on("result", (data) => {  
+        geocoderCallback(data);  
+    });
+    
+}
+
+/**
+ * Converts array of stores to GeoJSON format
+ * @param {Store[]} stores
+ * @return {StoresGeoJSON} Stores in GeoJSON
+ */
+
+export function convertToGeoJson(stores) {return {  
+    type: "FeatureCollection",  
+    features: stores.map(store => {  
+        return {  
+            type: "Feature",  
+            geometry: {  
+                type: 'Point',  
+                coordinates: [store.longitude, store.latitude]  
+            },  
+            properties: {  
+                id: store.id,  
+                name: store.name,  
+                address: store.address,  
+                phone: store.phone,  
+                distance: store.distance,  
+                rating: store.rating,  
+            }  
+        }  
+    })  
+  }}
+
+
+/**
+ * Display stores on map
+ * @param {Object} map
+ * @param {StoresGeoJSON} storesGeoJson
+ */
+export function plotStoresOnMap(map, storesGeoJson) {
+
+    for(let store of storesGeoJson.features) { 
+
+        // create a HTML element for each feature  
+        let el = document.createElement('div');  
+
+        el.className = 'store';
+
+        el.title = `${store.properties.name}\n` +  
+        `approximately ${store.properties.distance.toFixed(2)} km away\n` +  
+        `Address: ${store.properties.address || "N/A"}\n` +  
+        `Phone: ${store.properties.phone || "N/A"}\n` +  
+        `Rating: ${store.properties.rating || "N/A"}`; 
+        
+        // make a marker for each feature and add to the map  
+        new mapboxgl.Marker(el)  
+            .setLngLat(store.geometry.coordinates)  
+            .addTo(map); el.addEventListener('click', function(e) {  
+            updateSelectedStore(store.properties.id);  
+        });
+    }  
+    
+    
+}
+
+/**
+ * Zoom in-to a specific point on a map
+ * @param {Object} map
+ * @param {StoreFeatureObject} point
+ */
+export function flyToStore(map, point) {
+
+}
+
+/**
+ * Display store info on the map using a popup
+ * @param {Object} map
+ * @param {StoreFeatureObject} point
+ */
+export function displayStoreDetails(map, point) {
+    
+}
