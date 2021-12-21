042b275f7f65e8f26f8aa97a6603303531477f52
muhammad <fadi.anony@protonmail.ch>
2021-12-21 13:40:01 +0200

    change sores model

--- stores/models.py
+++ stores/models.py
@@ -7,17 +7,17 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # انشاء نموذج لتخزين بيانات المتاجر
 class Store(models.Model):
-    id = models.CharField(primary_key=True, max_length=100)
+    id = models.CharField(primary_key=True, max_length=65)
     created_at = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=65)
     rating = models.FloatField(null=True, validators=[MinValueValidator(0.0), MaxValueValidator(5.0)])
-    store_type = models.CharField(null=True, max_length=50)
+    store_type = models.CharField(null=True, max_length=65)
     opening_hour = models.TimeField(null=True)
     closing_hour = models.TimeField(null=True)
-    city = models.CharField(max_length=50)
+    city = models.CharField(max_length=65)
     latitude = models.FloatField(null=True)
     longitude = models.FloatField(null=True)
     location = models.PointField(null=True)
-    address = models.CharField(max_length=110)
-    phone = models.CharField(null=True, max_length=100)
+    address = models.CharField(max_length=65)
+    phone = models.CharField(null=True, max_length=65)
     
